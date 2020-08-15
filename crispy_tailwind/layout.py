f02d4c401f77a0a7826e5d2297c5c2fd0532bde2
Cody Fincher <cody.fincher@accenture.com>
2020-08-15 16:55:30 -0500

    altered button defaults

--- crispy_tailwind/layout.py
+++ crispy_tailwind/layout.py
@@ -15,7 +15,7 @@ class Submit(BaseInput):
 
     def __init__(self, *args, css_class=None, **kwargs):
         if css_class is None:
-            self.field_classes = "bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+            self.field_classes = "inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
         else:
             self.field_classes = css_class
         super().__init__(*args, **kwargs)
@@ -34,7 +34,7 @@ class Reset(BaseInput):
 
     def __init__(self, *args, css_class=None, **kwargs):
         if css_class is None:
-            self.field_classes = "bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+            self.field_classes = "inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:border-red-700 focus:shadow-outline-red active:bg-red-700 transition ease-in-out duration-150"
         else:
             self.field_classes = css_class
         super().__init__(*args, **kwargs)
@@ -53,7 +53,7 @@ class Button(BaseInput):
 
     def __init__(self, *args, css_class=None, **kwargs):
         if css_class is None:
-            self.field_classes = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            self.field_classes = "inline-flex items-center px-4 py-2 border border-transparent text-sm leading-5 font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-50 focus:outline-none focus:border-indigo-300 focus:shadow-outline-indigo active:bg-indigo-200 transition ease-in-out duration-150"
         else:
             self.field_classes = css_class
         super().__init__(*args, **kwargs)
